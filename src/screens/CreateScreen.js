14792031fc230b37c08d98134dda2dbf06077196
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-03-31 19:18:37 +0100

    Create CreateScreen.js

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
+
+const CreateScreen = ({ route, navigation }) => {
+
+  return (
+    <View>
+      <Text>CreateScreen</Text>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
