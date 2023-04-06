0a4d71d0310f448cc8536227a96134f067aa5622
Neba Emmanuel <nebaemmanuel736@gmail.com>
2023-04-06 03:17:17 +0100

    Update ShowScreen.js

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -12,8 +12,8 @@ const ShowScreen = ({ route, navigation }) => {
 
   return (
     <View>
-      <Text>{blogPost.title}</Text>
-      <Text>{blogPost.content}</Text>
+      <Text style={styles.title}>{blogPost.title}</Text>
+      <Text style={styles.content}>{blogPost.content}</Text>
     </View>
   );
 };
@@ -32,6 +32,18 @@ const ShowScreen = ({ route, navigation }) => {
 //   };
 // };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title:{
+    marginTop: 20,
+    fontSize: 32,
+    textAlign: 'center',
+    fontWeight: 600
+  },
+  content:{
+    marginTop: 5,
+    fontSize: 18,
+    textAlign: 'center',
+  }
+});
 
 export default ShowScreen;
